39e53f5a9879e01f4987644dab70ac0bbac0e915
Elmar Krainz <elmar.krainz@fh-joanneum.at>
2019-12-09 11:16:39 +0100

    factory example

---
+++ src/main/java/at/fhj/msd/swdesign/factorymethod/AnotherProduct.java
@@ -0,0 +1,11 @@
+package at.fhj.msd.swdesign.factorymethod;
+
+import at.fhj.msd.swdesign.factorymethod.Product;
+
+public class AnotherProduct extends Product {
+
+    public AnotherProduct(){
+        System.out.println(" another Product");
+    }
+
+}
