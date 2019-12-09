c6d33558d0c2594ccbdc2139f9262bd852e1171a
Elmar Krainz <elmar.krainz@fh-joanneum.at>
2019-12-09 11:17:01 +0100

    factory example

---
+++ src/main/java/at/fhj/msd/swdesign/factorymethod/FactoryMain.java
@@ -0,0 +1,18 @@
+package at.fhj.msd.swdesign.factorymethod;
+
+public class FactoryMain {
+
+
+    public static void main(String[] args) {
+
+
+        Product p ;
+        p = ProductFactory.createProduct(1);
+        p = ProductFactory.createProduct(0);
+
+
+
+
+
+    }
+}
